dbc0e61321394ea206e1dc3b97c7640de9507a6f
Evan Vassallo <evan.vassallo@me.com>
2016-03-03 18:59:40 -0500

    Begin writing desktop createPlaylist unit tests

--- Desktop/test/ca/mcgill/ecse321/group01/homeaudiosystem/persistence/TestHomeAudioSystemController.java
+++ Desktop/test/ca/mcgill/ecse321/group01/homeaudiosystem/persistence/TestHomeAudioSystemController.java
@@ -1,10 +1,15 @@
 package ca.mcgill.ecse321.group01.homeaudiosystem.persistence;
 
+import static org.junit.Assert.*;
+
 import java.io.File;
 
 import org.junit.After;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
+import ca.mcgill.ecse321.group01.homeaudiosystem.controller.HomeAudioSystemController;
+import ca.mcgill.ecse321.group01.homeaudiosystem.controller.InvalidInputException;
 import ca.mcgill.ecse321.group01.homeaudiosystem.model.Album;
 import ca.mcgill.ecse321.group01.homeaudiosystem.model.AlbumTracklist;
 import ca.mcgill.ecse321.group01.homeaudiosystem.model.Artist;
@@ -36,6 +41,35 @@ public class TestHomeAudioSystemController {
 	public void tearDown() throws Exception {
 		HomeAudioSystem has = HomeAudioSystem.getInstance();
 		has.delete();
+	}
+	
+	@Test
+	public void testCreatePlaylist() throws Exception {
+		HomeAudioSystem has = HomeAudioSystem.getInstance();
+		assertEquals(0, has.getPlaylists().size());
+		
+		String title = "Chill";
+		
+		HomeAudioSystemController hasController = new HomeAudioSystemController();
+		
+		try {
+			Playlist playlist = new Playlist(title);
+			hasController.createPlaylist(playlist);
+		} catch (InvalidInputException e) {
+			fail();
+		}
+		
+		
+		
+		
+	}
+	
+	
+	private void checkResultPlaylist(String title, HomeAudioSystem has2) {
+		assertEquals(1, has2.getPlaylists().size());
+		assertEquals(title, has2.getPlaylist(0).getName());
+		assertEquals(0, has2.getAlbums().size());
+		assertEquals(0, has2.getArtists().size());
 	}
 	
 }
