b57964501c188c2f6e2b80e5ea0f4262befd680a
Evan Vassallo <evan.vassallo@me.com>
2016-03-04 15:35:54 -0500

    Adjusted application.HAS for new model
    Changed name to avoid confusion with model.HAS

---
+++ Desktop/src/ca/mcgill/ecse321/group01/homeaudiosystem/application/HomeAudioSystemApp.java
@@ -0,0 +1,21 @@
+package ca.mcgill.ecse321.group01.homeaudiosystem.application;
+
+import ca.mcgill.ecse321.group01.homeaudiosystem.persistence.PersistenceHomeAudioSystem;
+import ca.mcgill.ecse321.group01.homeaudiosystem.view.HomeAudioSystemPage;
+
+public class HomeAudioSystemApp {
+
+	public static void main(String[] args) {
+		// load model
+		PersistenceHomeAudioSystem.loadHomeAudioSystemModel();
+
+		// start UI
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new HomeAudioSystemPage().setVisible(true);
+			}
+		});
+
+	}
+
+}
