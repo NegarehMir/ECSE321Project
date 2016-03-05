7939ced19592e9bfb85b4448a766287fab8646b6
Evan Vassallo <evan.vassallo@me.com>
2016-03-05 12:12:25 -0500

    Complete Java addSongToPlaylist unit test

--- Desktop/test/ca/mcgill/ecse321/group01/homeaudiosystem/persistence/TestHomeAudioSystemController.java
+++ Desktop/test/ca/mcgill/ecse321/group01/homeaudiosystem/persistence/TestHomeAudioSystemController.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.sql.Date;
+import java.util.LinkedList;
 
 import org.junit.After;
 import org.junit.BeforeClass;
@@ -11,6 +12,7 @@ import org.junit.Test;
 
 import ca.mcgill.ecse321.group01.homeaudiosystem.controller.HomeAudioSystemController;
 import ca.mcgill.ecse321.group01.homeaudiosystem.controller.InvalidInputException;
+import ca.mcgill.ecse321.group01.homeaudiosystem.controller.SongMetadata;
 import ca.mcgill.ecse321.group01.homeaudiosystem.model.Album;
 import ca.mcgill.ecse321.group01.homeaudiosystem.model.Artist;
 import ca.mcgill.ecse321.group01.homeaudiosystem.model.HomeAudioSystem;
@@ -124,23 +126,71 @@ public class TestHomeAudioSystemController {
 		has.addArtist(artist);
 		assertEquals(1, has.getArtists().size());
 		
+		LinkedList<SongMetadata> songsToAdd = new LinkedList<>();
+		songsToAdd.add(new SongMetadata("Countdown", 213));
+		
 		Date releaseDate = Date.valueOf("2011-06-24");
 		
-		HomeAudioSystemController hasController = new HomeAudioSystemController();
 		Album album = new Album("4", has, releaseDate, artist);
 		
-		Song song = new Song("Countdown", 213, album, artist);
+		Song song = new Song("4", 213, album, artist);
 		album.addSong(song);
 		assertEquals(1, album.getSongs().size());
 		
-		playlist.addSong(song);
-		assertEquals(1, playlist.getSongs().size());
+		HomeAudioSystemController hasController = new HomeAudioSystemController();
+		
+		try {
+			hasController.addSongToPlaylist(song, playlist);
+		} catch (InvalidInputException e) {
+			fail();
+		}
+		
+		checkResultAddSongToPlaylist("Countdown", 213, "Workout", has);
+		
+		HomeAudioSystem has2 = (HomeAudioSystem) PersistenceXStream.loadFromXMLwithXStream();
+		
+		checkResultAddSongToPlaylist("Countdown", 213, "Workout", has2);
+	}
+	
+	@Test
+	public void testAddSongToPlaylistNull() {
+		HomeAudioSystem has = HomeAudioSystem.getInstance();
+		
+		Playlist playlist = null;
+		assertEquals(0, has.getPlaylists().size());
+		
+		Song song = null;
+		String error = "";
+		HomeAudioSystemController hasController = new HomeAudioSystemController();
+		
+		try {
+			hasController.addSongToPlaylist(song, playlist);
+		} catch (InvalidInputException e) {
+			// TODO Auto-generated catch block
+			error = e.getMessage();
+		}
+		
+		assertEquals("Please choose a song! Please choose a playlist!", error);
+		
+		assertEquals(0, has.getPlaylists().size());
+		assertEquals(0, has.getArtists().size());
+		assertEquals(0, has.getLocations().size());
 	}
 	
 	
 	private void checkResultPlaylist(String title, HomeAudioSystem has2) {
 		assertEquals(1, has2.getPlaylists().size());
 		assertEquals(title, has2.getPlaylist(0).getTitle());
+		assertEquals(0, has2.getArtists().size());
+		assertEquals(0, has2.getLocations().size());
+	}
+	
+	private void checkResultAddSongToPlaylist(String songTitle, int songDuration, String playlistTitle, HomeAudioSystem has2) {
+		assertEquals(1, has2.getPlaylists().size());
+		assertEquals(1, has2.getPlaylist(0).getSongs().size());
+		assertEquals(playlistTitle, has2.getPlaylist(0).getTitle());
+		assertEquals(songTitle, has2.getPlaylist(0).getSong(0).getTitle());
+		assertEquals(songDuration, has2.getPlaylist(0).getSong(0).getDuration());
 		assertEquals(0, has2.getArtists().size());
 		assertEquals(0, has2.getLocations().size());
 	}
