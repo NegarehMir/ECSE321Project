07721cd63da12f837dc2f5b600753ac26f1fa398
Nicky <s.n.mirfallah@gmail.com>
2016-03-11 21:26:07 -0500

    Update from master

---
+++ Desktop/src/ca/mcgill/ecse321/group01/homeaudiosystem/model/LocationMusicItem.java
@@ -0,0 +1,11 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.22.0.5146 modeling language!*/
+package ca.mcgill.ecse321.group01.homeaudiosystem.model;
+
+public interface LocationMusicItem
+{
+  
+  // ABSTRACT METHODS 
+
+ public void play();
+}
