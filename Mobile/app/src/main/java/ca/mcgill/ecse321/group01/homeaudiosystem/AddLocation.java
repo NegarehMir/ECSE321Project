40bed0b3d4ba8c3f00c9a920975e065b2963eae8
Eva Suska <eva.suska@mail.mcgill.ca>
2016-03-16 15:17:33 -0400

    Removed unused mail button thing

--- Mobile/app/src/main/java/ca/mcgill/ecse321/group01/homeaudiosystem/AddLocation.java
+++ Mobile/app/src/main/java/ca/mcgill/ecse321/group01/homeaudiosystem/AddLocation.java
@@ -18,23 +18,12 @@ import ca.mcgill.ecse321.group01.homeaudiosystem.persistence.PersistenceHomeAudi
 
 public class AddLocation extends AppCompatActivity {
 
-    private String error = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         PersistenceHomeAudioSystem.setFileName(getFilesDir().getAbsolutePath() + "/" + "homeAudioSystem.xml");
         PersistenceHomeAudioSystem.loadHomeAudioSystemModel();
